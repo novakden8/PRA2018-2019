cf3bf65af5f7ac2c6950716ca4d401212cf2c6f2
s432837 <novakden8>
2018-10-04 13:03:14 +0200

    kek

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,16 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+    static Logger log = Logger.getLogger("abc");
+
     public static void main(String [ ] args) {
+
+        log.info("message");
+
         System.out.println("Hello World");
     }
+
 }
